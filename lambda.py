cf0f7385b8940306453c0ec49c196b7edfd147af
Saul Quispe <saul9412@hotmail.com>
2023-07-17 10:55:58 -0500

    test

--- lambda.py
+++ lambda.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def lambda_handler(event, context):
+def handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     return 'Done x1.1'
