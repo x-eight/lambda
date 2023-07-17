fd8cbf66af8408e897881aedf9a67c6946793e67
Saul Quispe <saul9412@hotmail.com>
2023-07-17 15:56:26 -0500

    import 6

--- lambda.py
+++ lambda.py
@@ -1,5 +1,16 @@
 from mangum import Mangum
-from src import main
+from fastapi import FastAPI
+#from src import main
 
-handler = Mangum(main.create_app())
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+handler = Mangum(app)
 
